380079c3fc3a303e382da2fdb91854d2a9b4743f
Nick Su <ponchi9620@gmail.com>
2018-04-29 23:30:24 +0800

    test db and line template

--- center.php
+++ center.php
@@ -5,28 +5,52 @@ require_once('db/Pdo.php');
 
 $client = new LINEBotTiny(LINE_CHANNELACCESSTOKEN, LINE_CHANNELSECRET);
 foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $message['text']."test"
-                            )
-                        )
-                    ));
-                    break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log("Unsupporeted event type: " . $event['type']);
-            break;
-    }
+	
+	$pdo = new ExtendPDO(DB_HOST,DB_DBNAME,DB_ENCODE,DB_USER,DB_PW);
+	$query = $pdo->select("SELECT 1 FROM accounts WHERE line_user_id=:line_user_id LIMIT 1",[
+		':line_user_id' => $event["source"]["userId"],
+	]);
+	$test = count($query);
+	if($event['type'] == 'message'){
+		$message = $event['message'];
+		if($message['type']=='text'){
+			if (strtolower($message['text']) == "template"){
+				$client->replyMessage(array(
+					'replyToken' => $event['replyToken'],
+					'messages' => array(
+						array(
+							'type' => 'template', // 訊息類型 (模板)
+							'altText' => 'Example buttons template', // 替代文字
+							'template' => array(
+								'type' => 'buttons', // 類型 (按鈕)
+								'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
+								'title' => 'Example Menu', // 標題 <不一定需要>
+								'text' => 'Please select', // 文字
+								'actions' => array(
+									array(
+										'type' => 'postback', // 類型 (回傳)
+										'label' => 'Postback example', // 標籤 1
+										'data' => 'action=buy&itemid=123' // 資料
+									),
+									array(
+										'type' => 'message', // 類型 (訊息)
+										'label' => 'Message example', // 標籤 2
+										'text' => 'Message example' // 用戶發送文字
+									),
+									array(
+										'type' => 'uri', // 類型 (連結)
+										'label' => 'Uri example', // 標籤 3
+										'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+									)
+								)
+							)
+						)
+					)
+				));
+			}
+			else{
+				$client->replyMessageSimple($event['replyToken'],$test.$message['text']."test5");
+			}
+		}
+	}
 };
--- models/LINEBotTiny.php
+++ models/LINEBotTiny.php
@@ -92,7 +92,38 @@ class LINEBotTiny
         return $data['events'];
     }
 
-    public function replyMessage($message)
+    public function replyMessageSimple($reply_token,$message_text)
+    {
+		$message = array(
+			'replyToken' => $reply_token,
+			'messages' => array(
+				array(
+					'type' => 'text',
+					'text' => $message_text
+				)
+			)
+		);
+        $header = array(
+            "Content-Type: application/json",
+            'Authorization: Bearer ' . $this->channelAccessToken,
+        );
+
+        $context = stream_context_create(array(
+            "http" => array(
+                "method" => "POST",
+                "header" => implode("\r\n", $header),
+                "content" => json_encode($message),
+            ),
+        ));
+
+        $response = file_get_contents('https://api.line.me/v2/bot/message/reply', false, $context);
+        if (strpos($http_response_header[0], '200') === false) {
+            http_response_code(500);
+            error_log("Request failed: " . $response);
+        }
+    }
+	
+	public function replyMessage($message)
     {
         $header = array(
             "Content-Type: application/json",
